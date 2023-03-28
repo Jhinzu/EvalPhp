fbe6d48082bb0ac4d352ae71729e1d2de50d2a56
Jhinzu <jeandetorres30140@gmail.com>
2023-03-28 09:29:14 +0200

    j'ai fais le paginator

--- src/Controller/AllAnnoncesController.php
+++ src/Controller/AllAnnoncesController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\AnnoncesRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,9 +12,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AllAnnoncesController extends AbstractController
 {
     #[Route('/annonces', name: 'app_all_annonces')]
-    public function index(AnnoncesRepository $annoncesRepo): Response
+    public function index(AnnoncesRepository $annoncesRepo, PaginatorInterface $paginator, Request $request): Response
     {
         $annonces = $annoncesRepo->findAll();
+
+        $annonces = $paginator->paginate(
+            $annonces, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            5 /*limit per page*/ );
 
         return $this->render('all_annonces/index.html.twig', [
             'controller_name' => 'AllAnnoncesController',
